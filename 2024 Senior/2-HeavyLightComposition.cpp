06f36e7bbbcd3d507158ba5ae6ace0030b1db732
dev0101110 <xefrom.xefrom@gmail.com>
2024-11-19 13:55:29 -0500

    Create 2-HeavyLightComposition.cpp

---
+++ 2024 Senior/2-HeavyLightComposition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio> // For scanf and printf
+
+using namespace std;
+
+int main() {
+    int N, C;
+    
+    // Use scanf to read two integers in a single line
+    scanf("%d %d", &N, &C);
+    
+    cout << N+C;
+    return 0;
+}
