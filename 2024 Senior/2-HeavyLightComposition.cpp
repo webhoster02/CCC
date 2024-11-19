11ea0006d20be8d03ef3f0e2ced89ae231dcba4d
dev0101110 <xefrom.xefrom@gmail.com>
2024-11-19 14:00:23 -0500

    Update 2-HeavyLightComposition.cpp

--- 2024 Senior/2-HeavyLightComposition.cpp
+++ 2024 Senior/2-HeavyLightComposition.cpp
@@ -9,6 +9,15 @@ int main() {
     // Use scanf to read two integers in a single line
     scanf("%d %d", &N, &C);
     
+    for (int i = 0; i < N; i++) {
+        char line[C];
+        for (int j = 0; j < C; j++) {
+            char ch;
+            scanf("%c", &ch);
+            line[j] = ch;
+        }
+    }
+    
     cout << N+C;
     return 0;
 }
