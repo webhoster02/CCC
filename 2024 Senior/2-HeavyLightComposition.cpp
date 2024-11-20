e6baa08b97b5e5712313c6c4ebf7b5af3e34a02d
webhoster02 <xefrom.xefrom@gmail.com>
2024-11-19 20:27:46 -0500

    Finished HeavyLightComposition

--- 2024 Senior/2-HeavyLightComposition.cpp
+++ 2024 Senior/2-HeavyLightComposition.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
 #include <cstdio> // For scanf and printf
+#include <vector>
 
 using namespace std;
+
+char scanLine(int N, int C) {
+    char output = 'T';
+    char line[C + 1];
+
+    // Find the heavy letters
+    vector<bool> foundOnce(26);
+    vector<bool> heavyLetters(26); // letters we know are heavy
+    for (int i = 1; i < C + 1; i++) {
+        // Get the character
+        char ch;
+        scanf(" %c", &ch);
+        if (output == 'F') continue; 
+        line[i] = ch;
+        
+        // early break: same character twice = not alternating for sure
+        if (line[i] == line[i-1])
+            output = 'F';
+
+        // Set the heavyletter of the character
+        if (!heavyLetters[ch-97]) {
+            if (!foundOnce[ch-97]) { 
+                foundOnce[ch-97] = true;
+            } else {
+                heavyLetters[ch-97] = true;
+            }
+        }
+        
+    }
+
+    if (output == 'F') return output;
+
+    bool firstHeavy = heavyLetters[line[1]-97];
+    for (int i = 1; i < C + 1; i++) {
+        char ch = line[i];
+        bool heavy = heavyLetters[ch-97];
+        if (heavy != (i % 2 == firstHeavy))
+            return 'F';
+    }
+    return output;
+}
 
 int main() {
     int N, C;
@@ -9,15 +51,16 @@ int main() {
     // Use scanf to read two integers in a single line
     scanf("%d %d", &N, &C);
     
+    char output[N];
+
     for (int i = 0; i < N; i++) {
-        char line[C];
-        for (int j = 0; j < C; j++) {
-            char ch;
-            scanf("%c", &ch);
-            line[j] = ch;
-        }
+        output[i] = scanLine(N, C);
+        getchar(); // Removes the newline
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << output[i] << "\n";
     }
     
-    cout << N+C;
     return 0;
 }
